ae4a411dfca1cc270a0469944c281838a76c595d
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-23 03:45:57 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,13 @@
+def a_method(a,b)
+  puts "I got #{a}"
+  puts "I got #{b}"
+  sum = a + b
+  puts "I got #{sum}"
+  sum
+end
+ 
+def multiply(num1, num2)
+  puts "multiply got called with #{num1} and #{num2}"
+  num1 * num2
+end
+multiply(1,2)
---
+++ hello_world.rb
@@ -0,0 +1,7 @@
+my_name = "Rachael"
+
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+greeting_a_person("Larry")
